e7a3f5d561577be9f85c6c81a3378c69f17a9532
Iyyafi Qolbi <iyyafi@gmail.com>
2023-10-24 14:48:56 +0700

    add session in client cookies

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,8 +5,23 @@
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider();
-    const user = await signInWithPopup(auth, provider);
-    console.log(user);
+    const credential = await signInWithPopup(auth, provider);
+
+    const idToken = await credential.user.getIdToken();
+
+    const res = await fetch("/api/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // 'CSRF-Token': csrfToken  // HANDLED by sveltekit automatically
+      },
+      body: JSON.stringify({ idToken }),
+    });
+  }
+
+  async function signOutSSR() {
+    const res = await fetch("/api/signin", { method: "DELETE" });
+    await signOut(auth);
   }
 </script>
 
@@ -14,8 +29,7 @@
 {#if $user}
   <h2 class="card-title">Welcome, {$user.displayName}</h2>
   <p class="text-center text-success">You are logged in</p>
-  <button class="btn btn-warning" on:click={() => signOut(auth)}
-    >Sign out</button
+  <button class="btn btn-warning" on:click={() => signOutSSR()}>Sign out</button
   >
 {:else}
   <button class="btn btn-primary" on:click={signInWithGoogle}
